01fd7436f858610fb1cb674f652b960ba68f28be
vanna sok <vannasok@vannas-MBP.fios-router.home>
2019-09-30 10:35:02 -0700

    import css file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import TodoList from './components/TodoComponents/TodoList';
 import TodoForm from './components/TodoComponents/TodoForm';
-import ReactDOM from 'react-dom';
-// import './App.css';
+import './App.css';
 // import './App.css';
 
 class App extends React.Component {
